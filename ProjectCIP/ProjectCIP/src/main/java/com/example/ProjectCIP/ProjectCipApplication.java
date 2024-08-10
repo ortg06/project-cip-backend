e7d4637e5d84d49c8a5e4bba8555ce1ba2e58ffa
Oscar Turish <ortg_10@hotmail.com>
2024-08-09 22:33:46 -0600

    commit configurations

---
+++ ProjectCIP/ProjectCIP/src/main/java/com/example/ProjectCIP/ProjectCipApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCipApplication.class, args);
+	}
+
+}
---
+++ ProjectCIP/ProjectCIP/src/test/java/com/example/ProjectCIP/ProjectCipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
